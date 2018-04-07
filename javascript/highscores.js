0c00d59a306db9ad383737ab26b4dce8d8a141d9
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-04-07 21:53:10 +0200

    Implemented correct styles for the ratings representation

--- javascript/highscores.js
+++ javascript/highscores.js
@@ -23,15 +23,17 @@ function updateHighstore() {
 
   const data = document.getElementById("highscores-data");
   if (highscores.length > 0) {
-    let list = '<ol>';
-    highscores.forEach(function(element) {
-      list += '<li>';
-      list += '<span>' + element.name + ': </span>';
-      list += '<span>' + element.result + '</span>';
-      list += '</li>';
+    let table = '<table class="highscore-records">';
+    table += '<tr><th>Position</th><th>Name</th><th>Result</th></tr>';
+    highscores.forEach(function(element, index) {
+      table += '<tr>';
+      table += '<td>' + (index + 1) + '</td>';
+      table += '<td class="record-name">' + element.name + '</td>';
+      table += '<td>' + element.result + '</td>';
+      table += '</tr>';
     });
-    list += '</ol>';
-    data.innerHTML = list;
+    table += '</table>';
+    data.innerHTML = table;
   } else {
     const message = '<h3>There is no any records here yet</h3>';
     data.innerHTML = message;
